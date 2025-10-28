953cc6d211b5309fe3b309cf6a0127fed19aa243
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-10-27 23:32:15 -0600

    basic setup and structure in hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import icon from '../assets/Icon.svg'
+import styles from '../styles/Hero.module.css'
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <img src={icon} alt="Neza Startup Logo" className={styles.icon} />
+      <p>Próximamente...¡Estamos trabajando en el desarrollo del sitio web oficial!</p>
+      <p>&#40;conforme el emprendimiento avance&#47;crezca&#41;</p>
+      <span>- Neza Team</span>
+    </section>
+  )
+}
+
+export default Hero
