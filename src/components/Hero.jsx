9877b67ec05917c258c2d65be21cfd774d62e2bf
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-05-30 22:24:34 -0600

    develop branch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,17 @@
-import icon from '../assets/icon.svg'
-import styles from '../styles/Hero.module.css'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import icon from '../assets/icon.svg';
+import styles from '../styles/Hero.module.css';
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <img src={icon} alt="Neza Startup Logo" className={styles.icon} />
-      <p>Próximamente...¡Estamos trabajando en el desarrollo del sitio web oficial!</p>
-      <p>&#40;conforme el emprendimiento avance&#47;crezca&#41;</p>
-      <span>- Neza Team</span>
+      <h1>Neza Startup</h1>
+      <h2>The Star Marketing & Software Startup</h2>
+      <div className={styles.ctaContainer}>
+        <a href="#contact" className={styles.cta}>View services <FontAwesomeIcon icon={faAngleRight} className={styles.icon} /> </a>
+        <a href="#contact" className={styles.ctaAux}>Contact us</a>
+      </div>
     </section>
   )
 }
