7c3aca965f68dd8d185ca963d1ae7cdc252c0cff
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-07-20 12:55:43 -0600

    Added share page functionality

--- src/components/SocialPath.jsx
+++ src/components/SocialPath.jsx
@@ -1,12 +1,31 @@
 import { faFacebook, faGithub, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faCalendar, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faEnvelope, faLink, faPhone, faShare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from '../styles/SocialPath.module.css';
 
 const SocialPath = () => {
+  const handleShare = () => {
+    const shareData = {
+      title: 'Neza Startup',
+      text: 'Check out Neza Startup!',
+      url: 'https://www.nezastartup.com',
+    };
+
+    if (navigator.share) {
+      navigator.share(shareData)
+        .then(() => console.log('Shared successfully'))
+        .catch((error) => console.error('Error sharing:', error));
+    } else {
+      alert('Sharing is not supported in this browser.');
+    }
+  };
+
   return (
     <div className={styles.socialPath}>
+      <a href="https://www.nezastartup.com/link-in-bio" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+        <FontAwesomeIcon icon={faLink} className={styles.icon} />
+      </a>
       <a href="https://wa.me/527771411554" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
         <FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />
       </a>
@@ -28,6 +47,9 @@ const SocialPath = () => {
       <a href="https://github.com/neza-startup" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
         <FontAwesomeIcon icon={faGithub} className={styles.icon} />
       </a>
+      <span onClick={handleShare} className={styles.socialLink}>
+        <FontAwesomeIcon icon={faShare} className={styles.icon} />
+      </span>
     </div>
   );
 };
