2ac507ec3b392b87b9d976d530bd09923b9a394b
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-07-23 06:08:53 -0600

    Added Q&A section

---
+++ src/components/Q&A.jsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Q&A.module.css';
+
+const QnA = () => {
+  return (
+    <section className={styles.qna}>
+      <header>
+        <h1>Q&A</h1>
+        <h2>Subheading</h2>
+      </header>
+    </section>
+  );
+};
+
+export default QnA;
