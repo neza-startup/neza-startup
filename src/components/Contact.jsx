93b1e687e694f8d088482447ace8e604896731b6
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-08-02 13:07:23 -0600

    Contact form api changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
 
     const sendEmailAndWhatsApp = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/contact', {
+        const response = await fetch('/api/contact', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
