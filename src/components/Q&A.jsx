6f6d35c0cfe8ae6caf39f18b5e2cad823e4a8c69
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-08-06 05:59:24 -0600

    Updated qna
    improved styles

--- src/components/Q&A.jsx
+++ src/components/Q&A.jsx
@@ -63,7 +63,7 @@ const QnA = () => {
     <section className={styles.qna} id="qna">
       <header className={styles.header}>
         <h1>Q&A</h1>
-        <span>Star Questions, Clear Answers</span>
+        <h2>Star Questions, Clear Answers</h2>
       </header>
       <div className={styles.questions}>
         {questions.map((q, index) => (
