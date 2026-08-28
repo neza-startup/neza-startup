63f924d758da13d4a2ad10e778e0dfce517af99d
elvisscochito <elvirodominguezsoriano@outlook.com>
2026-08-28 01:43:40 -0600

    added other services
    commented

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -61,46 +61,76 @@ const Services = () => {
           <FontAwesomeIcon icon={faStar} className={styles.icon} />
           <a href="/form"/* "https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */>Contact us{/* Learn More */}</a>
         </div>
-        {/* <h3>Phases</h3> */}
         {/* <div className={styles.serviceItem}>
+          <header>
+            <h3>Cloud and DevOps</h3>
+          </header>
+          <p>We provide cloud and DevOps services to help businesses streamline their operations and improve their efficiency.</p>
+          <FontAwesomeIcon icon={faStar} className={styles.icon} />
+          <a href="/form" */}{/* "https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a>
+        </div> */}
+        {/* <div className={styles.serviceItem}>
+          <header>
+            <h3>Cybersecurity</h3>
+          </header>
+          <p>We offer cybersecurity services to help businesses protect their data and systems from cyber threats.</p>
+          <FontAwesomeIcon icon={faStar} className={styles.icon} />
+          <a href="/form" */}{/* "https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a>
+        </div> */}
+        {/* <div className={styles.serviceItem}>
+          <header>
+            <h3>Networking</h3>
+          </header>
+          <p>We provide networking services to help businesses establish and maintain reliable and secure network infrastructures.</p>
+          <FontAwesomeIcon icon={faStar} className={styles.icon} />
+          <a href="/form" */}{/* "https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a> */}
+        {/* </div> */}
+      </div>
+      {/* <div className={styles.servicesPhases}>
+        <h1>Our Services Phases</h1>
+        <h2>We follow a structured approach to deliver high-quality services to our clients.</h2>
+      </div>
+      <div className={styles.servicePhasesList}> */}
+      {/* <h3>Phases</h3> */}
+      {/* <div className={styles.serviceItem}>
           <header>
             <h3>Consulting</h3>
           </header>
           <p>We assess viability, identify opportunities, recommend services and tools, and provide expert guidance. */}{/* We provide expert consulting services to help businesses navigate challenges and achieve their goals. */}{/* </p> */}
-        {/* <FontAwesomeIcon icon={faStar} className={styles.icon} />
+      {/* <FontAwesomeIcon icon={faStar} className={styles.icon} />
           <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
-        {/* </div> */}
-        {/* <div className={styles.serviceItem}>
+      {/* </div> */}
+      {/* <div className={styles.serviceItem}>
           <header>
             <h3>Design */}{/*  and Development */}{/* </h3>
           </header>
           <p>We create visually appealing and user-friendly designs that help businesses establish a strong brand identity.</p> */}
-        {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
-        {/* </div> */}
-        {/* <div className={styles.serviceItem}>
+      {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
+      {/* </div> */}
+      {/* <div className={styles.serviceItem}>
           <header>
             <h3>Project Management</h3>
           </header>
           <p>We provide comprehensive project management services to ensure that your projects are delivered on time, within budget, and to the highest quality standards.</p> */}
-        {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
-        {/* <a href="/form" */}{/*"https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a>
+      {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
+      {/* <a href="/form" */}{/*"https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a>
         </div> */}
-        {/* <div className={styles.serviceItem}>
+      {/* <div className={styles.serviceItem}>
           <header>
             <h3>Testing and Quality Assurance</h3>
           </header>
           <p>We offer rigorous testing and quality assurance for reliable, high-performing standard products.</p> */}
-        {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
-        {/* </div> */}
-        {/* <div className={styles.serviceItem}>
+      {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
+      {/* </div> */}
+      {/* <div className={styles.serviceItem}>
           <header>
             <h3>Deployment and Maintenance</h3>
           </header>
           <p>We provide ongoing support and maintenance for our products to ensure they continue to perform at their best.</p> */}
-        {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
-        {/* <a href="/form" */}{/* "https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a>
+      {/* <FontAwesomeIcon icon={faStar} className={styles.icon} /> */}
+      {/* <a href="/form" */}{/* "https://www.example.com/web-development" target="_blank" rel="noopener noreferrer" */}{/* >Contact us */}{/* Learn More */}{/* </a>
         </div> */}
-      </div>
+      {/* </div> */}
       <div className={styles.servicesModalities}>
         <h1>Our {/* Services  */}Modalities</h1>
         <h2>Find the modality that works best for you{/* We offer our services in various modalities to cater to the unique needs of our clients. */}</h2>
